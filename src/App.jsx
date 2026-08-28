67cc923d58729218483cc46b79a582f766eda907
yeigen <gabriel.dev.edwards@gmail.com>
2026-08-28 17:35:42 -0500

    chore: dejando la app en blanco

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import './App.css'
-import HolaMundo from './components/HolaMundo'
-import Bienvenida from './components/Bienvenida'
-import Contador from './components/Contador'
-import Arrays from './components/Arrays'
-import './index.css'
-
 function App() {
-  return (
-    <div>
-      <HolaMundo/>
-      <Arrays/>
-      <Bienvenida texto="HABLA HABLAME CLARO PORFA DEJA EL DESCARO"/>
-      <Contador/>
-    </div>
-  )
+  return null
 }
 
 export default App
--- src/components/Bienvenida.jsx
+++
@@ -1,9 +0,0 @@
-function Bienvenida({texto}) {
-  return (
-    <div className="hola-mundo">
-        <h2>BIENVENIDOOOOOOOOOO {texto} </h2>
-    </div>
-  )
-}
-
-export default Bienvenida
