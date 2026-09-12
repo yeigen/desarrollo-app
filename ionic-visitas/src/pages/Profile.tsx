e2cc1cd26f6e0cdc1b5fe5cb4e370dff8743b5f5
yeigen <gabriel.dev.edwards@gmail.com>
2026-09-12 16:13:19 -0500

    feat: perfil para una vez que uno se loguee

--- ionic-visitas/src/pages/Profile.tsx
+++ ionic-visitas/src/pages/Profile.tsx
@@ -1,6 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import { logOutOutline } from 'ionicons/icons'
+import type { Session } from '../services/auth'
 
-function Profile() {
+interface ProfileProps {
+  session: Session
+  onLogout: () => void
+}
+
+function Profile({ session, onLogout }: ProfileProps) {
   return (
     <IonPage>
       <IonHeader>
@@ -8,7 +26,27 @@ function Profile() {
           <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen />
+      <IonContent className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonLabel>
+              <h2>{session.name}</h2>
+              <p>{session.specialty}</p>
+            </IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              <h2>Usuario</h2>
+              <p>{session.username}</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+
+        <IonButton className="ion-margin-top" expand="block" color="danger" onClick={onLogout}>
+          <IonIcon slot="start" icon={logOutOutline} />
+          Cerrar sesión
+        </IonButton>
+      </IonContent>
     </IonPage>
   )
 }
